086e47b4dc8be5d7356209c819d815a50ede21fa
Zhe Wang <weswangw@gmail.com>
2025-10-27 03:37:34 +0000

    Remove terraform directory (not part of submission)

--- terraform/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" { default = "us-east-1" }
-variable "account_id" { default = "654654181039" }
-variable "key_name" { default = null }
-
-# TEMP creds for EC2 to pull from ECR during boot
-variable "aws_access_key_id" { default = "PASTE_YOUR_CURRENT_ACCESS_KEY_ID" }
-variable "aws_secret_access_key" { default = "PASTE_YOUR_CURRENT_SECRET_ACCESS_KEY" }
-variable "aws_session_token" { default = "PASTE_YOUR_CURRENT_SESSION_TOKEN" }
